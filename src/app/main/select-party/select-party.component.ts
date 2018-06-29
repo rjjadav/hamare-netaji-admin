89b49f63bf7a09243020a73f8e2a3e73e61f3b9a
samirmakhani045 <samirmakhani045@gmail.com>
2018-06-29 12:11:56 +0530

    validation is added for select party component

--- src/app/main/select-party/select-party.component.ts
+++ src/app/main/select-party/select-party.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import {ToastrService} from 'ngx-toastr';
+import { ToastrService } from 'ngx-toastr';
 
 
 @Component({
@@ -26,14 +26,23 @@ export class SelectPartyComponent implements OnInit {
 
   initPartyForm() {
     this.partyForm = this.formBuilder.group({
-      name: [ null, Validators.required],
+      name: [null, Validators.required],
       active: [null, Validators.required]
     })
   }
-  addParty(formValues) {
-    this.httpClient.post('http://139.162.53.4/netaji/admin/addParty', formValues)
-      .subscribe((res) => {
-        this.toastrService.success('Party added sucessfully', 'Success');
-      });
+  addParty(partyForm) {
+    if (!partyForm.valid) {
+      Object.keys(partyForm.controls).forEach(field => {
+        const control = partyForm.get(field);
+        control.markAsTouched({ onlySelf: true });
+      })
+    } else {
+      this.httpClient.post('http://139.162.53.4/netaji/admin/addParty', partyForm.value)
+        .subscribe((res) => {
+          this.toastrService.success('Party added sucessfully', 'Success');
+        }, error => {
+          this.toastrService.error('please try after sometime', 'Failure');
+        });
+    }
   }
 }
