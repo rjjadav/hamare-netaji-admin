a1a51bba9e5b96a80a30cff9911d3e1ab0e40288
samirmakhani045 <samirmakhani045@gmail.com>
2018-07-02 18:50:03 +0530

    interview delete API done

--- src/app/main/interview-list/interview-list.component.ts
+++ src/app/main/interview-list/interview-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import {ToastrService} from 'ngx-toastr';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-party-list',
@@ -12,12 +12,13 @@ export class InterviewListComponent implements OnInit {
   interviewList;
   constructor(
     private httpClient: HttpClient,
+    private toastrService: ToastrService,
   ) { }
 
   ngOnInit() {
-    this.getParty();
+    this.getInterview();
   }
-  getParty() {
+  getInterview() {
     /*const token = JSON.parse(window.localStorage.getItem('token')).result;
     let headers = new HttpHeaders({ 'Authorization': `Bearer ${token['access_token']}` });
     let options = {
@@ -28,5 +29,16 @@ export class InterviewListComponent implements OnInit {
         this.interviewList = res['interviews'];
       });
   }
-
+  deleteInterview(id) {
+    var result = confirm("Do you really want to delete?");
+    if (result) {
+      this.httpClient.get('http://139.162.53.4/netaji/admin/deleteInterview?id='+id)
+      .subscribe((res) => {
+        this.toastrService.success('Interview deleted Successfully', 'Failure');
+       this.getInterview();
+      },(error)=>{
+      this.toastrService.error('Failure deleing Interview', 'Failure');
+      });
+    }
+  }
 }
